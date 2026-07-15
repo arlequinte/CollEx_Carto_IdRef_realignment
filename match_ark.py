1e0758f43808bc52e98cc5769d27ec7a9c2e6810
Anne Bugner <118920815+arlequinte@users.noreply.github.com>
2026-07-15 19:34:10 +0200

    Update match_ark.py
    Adding try/except + variations of 'IdRef' mentions

--- match_ark.py
+++ match_ark.py
@@ -32,7 +32,9 @@ def main():
     
     # Compteurs
     script_counter = 0
-    elements_réindexés = []
+    elements_réindexés_bnf = []
+    elements_corriges_idref = []
+    erreurs_http = []
     
     # Ouverture fichiers
     for ead in iglob(os.path.join(path, extension)):
@@ -42,9 +44,12 @@ def main():
         
         # Identification du fichier source et du producteur de la notice
         filename = informations_generales_fonds(ET.XPath("//eadid"), notice)
+
+        # Correction des mentions d'IdRef erronées en @source
+        fix_idref_sourcing(notice, filename, elements_corriges_idref)
         
         # Fonction centrale de réindexation
-        reindexation(notice, '//*[@source="BnF"]', indices_absents_idref, filename, elements_réindexés)
+        reindexation(notice, '//*[@source="BnF"]', indices_absents_idref, filename, elements_réindexés_bnf)
         
         ### Enregistrer l'arborescence modifiée dans un nouveau fichier 
         make_xml(notice, filename) 
@@ -53,15 +58,39 @@ def main():
         print(f"Fichier : {script_counter}/1243", end='\r') # Remplacer "1243" par le nombre total de fichiers dans notices_collex, s'il a changé
     
     if indices_absents_idref != []:
-        make_csv_indices_absents(indices_absents_idref)
-        print("Tous les éléments indexés n'ont pas pu être identifiés dans IdRef !")        
+        with open("databnf_sans_idref.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            writer.writerow(["Nom du fichier source", "Provenance", "Nom du fonds", "URI index", "Label"])
+            writer.writerows(indices_absents_idref)
+        print(f"{len(indices_absents_idref} éléments catalogués dans data.bnf n'ont pas l'air d'avoir de lien vers IdRef !")  
+        
+    if erreurs_http != []:
+        with open("erreurs_requetes_databnf.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            writer.writerows(erreurs_http)    
+        print(f"{len(erreurs_http)} identifiants ARK ont renvoyé une erreur de requête sur data.bnf")
     
     print("")
-    print(f"Nombre d'éléments réindexés avec succès : {len(elements_réindexés)}")
-    print(f"Nombre d'éléments laissés tels quels : {len(indices_absents_idref)}")
+    print(f"Nombre d'éléments @source='BnF' réindexés avec succès : {len(elements_réindexés_bnf)}")
+    print(f"Nombre de mentions 'idref' baroques corrigées : {len(elements_corriges_idref)}")
+    print(f"Nombre d'éléments @source='BnF' laissés tels quels : {len(indices_absents_idref)}")
     
+def fix_idref_sourcing(notice, filename, elements_corriges_idref):
+    indexes = notice.xpath("//*[@source='Sudoc' or @source='SUDOC' or @source='idRef' or @source='idref' or @source='Idref']")
+    if indexes != []:
+        for index in indexes:
+            index.set('source', 'IdRef')
+            ppn = index.get("authfilenumber") 
+            if len(ppn) == 9:
+                ppn = "https://www.idref.fr/" + ppn
+                index.set('authfilenumber', ppn)
+            else:
+                ppn = ppn.replace("http:", "https:") 
+            index.set('authfilenumber', ppn)   
+            elements_corriges_idref.append(ppn)    
+    return notice, elements_corriges_idref    
 
-def reindexation(notice, expression, indices_absents_idref, filename, elements_réindexés):
+def reindexation(notice, expression, indices_absents_idref, filename, elements_réindexés_bnf):
     
     ### Chercher les indexations BnF
     elements_index_BnF = notice.xpath(expression)
@@ -72,8 +101,14 @@ def reindexation(notice, expression, indices_absents_idref, filename, elements_r
             ark = element.get('authfilenumber').strip() # On est pas à l'abri de trailing spaces inopinés
 
             # Vérifier qu'on ne se trouve pas en présence d'erreurs qui inscrivent ensemble @source="BnF" et @authfilenumber = PPN IdRef
-            if "https://www.idref.fr/" in ark:
+            if "www.idref.fr/" in ark:
                 element.set("source", "IdRef")
+                ppn_idref = ark.replace("http:", "https:")
+                element.set("authfilenumber", ppn_idref)
+            elif "http://ark.bnf.fr/" in ark:
+                uri_ark = ark.replace("http://ark.bnf.fr/", "http://data.bnf.fr/")    
+            elif "data.bnf.fr" in ark:
+                uri_ark = ark  
             else:
                 uri_ark = "http://data.bnf.fr/" + ark
 
@@ -93,30 +128,34 @@ def reindexation(notice, expression, indices_absents_idref, filename, elements_r
                 sparql.setQuery(sparql_query)
                 sparql.setReturnFormat(JSON)
 
-                result = sparql.query().convert()
-                
-                if result["results"]["bindings"]:
-                        
-                    uri_idref = result["results"]["bindings"][0]["idrefUri"]["value"]
-                    ## Nettoyage valeur
-                    ppn_idref = uri_idref.strip("/id")
-                    ppn_idref = ppn_idref.replace("http:", "https:")
-                        
-                    ### Ajout du résultat dans l'élément d'origine
-                    element.set("authfilenumber", ppn_idref)
-                    element.set("source", "IdRef")
-                    elements_réindexés.append(uri_idref)
-                # Traitement des index absents d'IDRef (ex : Hanoi, dont la notice IDRef 027291502 est associée à ark:/12148/cb15292667g, et non pas ark:/12148/cb11936556x, choisi par CollEx)
-                else:
-                    # Infos tableur de suivi : nom du fichier, ark concerné, label, nom du fonds, nom établissement
-                    nom_fonds = informations_generales_fonds(ET.XPath("//unittitle"), notice)
-                    provenance = informations_generales_fonds(ET.XPath("//repository/corpname"), notice)
-                    label = element.get("normal")
-                    if label is None:
-                        label = element.text
-                    indices_absents_idref.append([filename, provenance, nom_fonds, uri_ark, label])
+                try:
+                    result = sparql.query().convert()
+                    
+                    if result["results"]["bindings"]:
+                            
+                        uri_idref = result["results"]["bindings"][0]["idrefUri"]["value"]
+                        ## Nettoyage valeur
+                        ppn_idref = uri_idref.strip("/id")
+                        ppn_idref = ppn_idref.replace("http:", "https:")
+                            
+                        ### Ajout du résultat dans l'élément d'origine
+                        element.set("authfilenumber", ppn_idref)
+                        element.set("source", "IdRef")
+                        elements_réindexés_bnf.append(uri_idref)
+                    # Traitement des index absents d'IDRef (ex : Hanoi, dont la notice IDRef 027291502 est associée à ark:/12148/cb15292667g, et non pas ark:/12148/cb11936556x, choisi par CollEx)
+                    else:
+                        # Infos tableur de suivi : nom du fichier, ark concerné, label, nom du fonds, nom établissement
+                        nom_fonds = informations_generales_fonds(ET.XPath("//unittitle"), notice)
+                        provenance = informations_generales_fonds(ET.XPath("//repository/corpname"), notice)
+                        label = element.get("normal")
+                        if label is None:
+                            label = element.text
+                        indices_absents_idref.append([filename, provenance, nom_fonds, uri_ark, label])
+                except:
+                    erreurs_http.append([filename, uri_ark])
+                    continue
                            
-    return notice, indices_absents_idref, elements_réindexés    
+    return notice, indices_absents_idref, elements_réindexés_bnf    
 
 def informations_generales_fonds(XPath, notice):
     find_info = XPath(notice)
@@ -126,12 +165,5 @@ def informations_generales_fonds(XPath, notice):
 def make_xml(notice, filename):
     with open('Dump_xmlead/' + filename, "w", encoding="utf-8") as f:
         ccfr_new = f.write(ET.tostring(notice, xml_declaration=True, doctype='<!DOCTYPE ead PUBLIC "+//ISBN 1-931666-00-8//DTD ead.dtd (Encoded Archival Description (EAD) Version 2002)//EN" "ead.dtd">', pretty_print=True, encoding='utf-8').decode('utf-8')) 
-
-def make_csv_indices_absents(indices_absents_idref):
-    with open("indices_absents_de_data_bnf.csv", mode="w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, delimiter='\t')
-        writer.writerow(["Nom du fichier source", "Nom du fonds", "Provenance", "URI index", "Label"])
-        writer.writerows(indices_absents_idref)
-        return csvfile
 
 main()
